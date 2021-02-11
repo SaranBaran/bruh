9b6a7e30809767330f5b45de8fed884a077d29f9
SaranBaran <69807636+SaranBaran@users.noreply.github.com>
2021-02-12 00:34:33 +0300

    yeş

--- Assets/Scripts/CollectColidesNotCollect.cs
+++ Assets/Scripts/CollectColidesNotCollect.cs
@@ -7,9 +7,15 @@ public class CollectColidesNotCollect : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D notCollect)
     {
-
         Vector3 originalPos = transform.localPosition;
-        float x = originalPos.x + 2f;
+        float x = originalPos.x + 1f;
+        transform.localPosition = new Vector3(x, originalPos.y, originalPos.z);
+    }
+
+    void OnCollisionEnter(Collision walls)
+    {
+        Vector3 originalPos = transform.localPosition;
+        float x = originalPos.x - 3f;
         transform.localPosition = new Vector3(x, originalPos.y, originalPos.z);
     }
 
